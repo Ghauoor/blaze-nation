727b39429fbe71f7fc191d87a64bda62831c16b2
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-05-10 16:43:56 +0500

    Import file for the component

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Header from "./common/Header";
+import Footer from "./common/Footer";
+import Button from "./common/Button";
+import Section from "./Section";
+
+export { Header, Footer, Button, Section };
